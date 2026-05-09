ced4bcd2de6826e9fc9bd5164795e909cb7107db
Lura <maksimonichshenko2906@gmail.com>
2026-05-09 11:45:50 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Browser_New
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
